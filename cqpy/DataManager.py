2acc6d4502e893b2dc45043b20dfb8beea28fdd7
xyazh <330237917@qq.com>
2023-12-24 16:06:00 +0800

    修改了data保存的位置

--- cqpy/DataManager.py
+++ cqpy/DataManager.py
@@ -1,40 +1,50 @@
-import os,json,io,logging
+import os
+import json
+import io
+import logging
+
 
 class DataManager:
     def __init__(self, path="\\cqpy_data\\"):
         current_directory = os.getcwd()
         parent_directory = os.path.dirname(current_directory)
         self.path = os.path.join(parent_directory, path)
+        self.path = parent_directory+"\\SSJtoMeki_data"
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        self.path += path
         if not os.path.exists(self.path):
             os.mkdir(self.path)
 
-    def getFileIO(self,file_name:str,type:str="rb")->io.IOBase:
+    def getFileIO(self, file_name: str, type: str = "rb") -> io.IOBase:
         full_path = self.path + file_name
         if os.path.exists(full_path):
-            f = open(full_path,type)
+            f = open(full_path, type)
             return f
         return None
 
-    def getFileFullPath(self,file_name:str)->str:
+    def getFileFullPath(self, file_name: str) -> str:
         full_path = self.path + file_name
         if not os.path.exists(full_path):
-            with open(full_path,"wb") as f:
-                f.write(json.dumps({},ensure_ascii=False,indent=4).encode("utf8"))
+            with open(full_path, "wb") as f:
+                f.write(json.dumps({}, ensure_ascii=False,
+                        indent=4).encode("utf8"))
 
         return full_path
-    def hasFile(self,file_name:str)->bool:
+
+    def hasFile(self, file_name: str) -> bool:
         full_path = self.path + file_name
         return os.path.exists(full_path)
 
-    def craftFile(self,file_name:str)->None:
+    def craftFile(self, file_name: str) -> None:
         full_path = self.path + file_name
-        with open(full_path,"wb") as f:
-                f.write(json.dumps({},ensure_ascii=False,indent=4).encode("utf8"))
+        with open(full_path, "wb") as f:
+            f.write(json.dumps({}, ensure_ascii=False, indent=4).encode("utf8"))
 
-    def get(self,file_name:str,key:str=None)->dict|list|int|str|float|bool|None:
+    def get(self, file_name: str, key: str = None) -> dict | list | int | str | float | bool | None:
         j = None
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
+            with open(self.getFileFullPath(file_name), "rb") as f:
                 j = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
@@ -45,35 +55,36 @@ class DataManager:
                 return j[key]
         return None
 
-    def findGet(self,file_name:str,key:str=None,dis_val=None):
-        r = self.get(file_name,key)
+    def findGet(self, file_name: str, key: str = None, dis_val=None):
+        r = self.get(file_name, key)
         if r == None:
             return dis_val
         return r
 
-    def set(self,file_name:str,key:str,val:dict|list|int|str|float|bool)->bool:
+    def set(self, file_name: str, key: str, val: dict | list | int | str | float | bool) -> bool:
         j = None
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
-                j:dict = json.loads(f.read())
+            with open(self.getFileFullPath(file_name), "rb") as f:
+                j: dict = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
-        if j!=None:
+        if j != None:
             if type(j) == dict:
                 j[key] = val
                 try:
-                    with open(self.getFileFullPath(file_name),"wb") as f:
-                        f.write(json.dumps(j,ensure_ascii=False,indent=4).encode("utf8"))
+                    with open(self.getFileFullPath(file_name), "wb") as f:
+                        f.write(json.dumps(j, ensure_ascii=False,
+                                indent=4).encode("utf8"))
                     return True
                 except BaseException as e:
                     logging.exception(e)
         return False
 
-    def getMenbers(self, file_name:str, keys:None|list[str]|tuple[str]=None, dis_fnl:object=lambda x:None)->dict:
+    def getMenbers(self, file_name: str, keys: None | list[str] | tuple[str] = None, dis_fnl: object = lambda x: None) -> dict:
         j = None
         r = {}
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
+            with open(self.getFileFullPath(file_name), "rb") as f:
                 j = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
@@ -88,20 +99,21 @@ class DataManager:
                     r[i] = dis_fnl(i)
         return r
 
-    def setMenbers(self, file_name:str, key_vals:dict)->bool:
+    def setMenbers(self, file_name: str, key_vals: dict) -> bool:
         j = None
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
-                j:dict = json.loads(f.read())
+            with open(self.getFileFullPath(file_name), "rb") as f:
+                j: dict = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
-        if j!=None:
+        if j != None:
             if type(j) == dict:
                 for key in key_vals:
                     j[key] = key_vals[key]
                 try:
-                    with open(self.getFileFullPath(file_name),"wb") as f:
-                        f.write(json.dumps(j,ensure_ascii=False,indent=4).encode("utf8"))
+                    with open(self.getFileFullPath(file_name), "wb") as f:
+                        f.write(json.dumps(j, ensure_ascii=False,
+                                indent=4).encode("utf8"))
                     return True
                 except BaseException as e:
                     logging.exception(e)
--- cqpy/xyazhServer/DataManager.py
+++ cqpy/xyazhServer/DataManager.py
@@ -1,28 +1,37 @@
-import os,json,io,logging
+import os
+import json
+import io
+import logging
+
 
 class DataManager:
     def __init__(self, path="\\cqpy_data\\"):
         current_directory = os.getcwd()
         parent_directory = os.path.dirname(current_directory)
         self.path = os.path.join(parent_directory, path)
+        self.path = parent_directory+"\\SSJtoMeki_data"
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        self.path += path
         if not os.path.exists(self.path):
             os.mkdir(self.path)
 
-    def getFileFullPath(self,file_name:str)->str:
+    def getFileFullPath(self, file_name: str) -> str:
         full_path = self.path + file_name
         if not os.path.exists(full_path):
-            with open(full_path,"wb") as f:
-                f.write(json.dumps({},ensure_ascii=False,indent=4).encode("utf8"))
+            with open(full_path, "wb") as f:
+                f.write(json.dumps({}, ensure_ascii=False,
+                        indent=4).encode("utf8"))
         return full_path
 
-    def hasFile(self,file_name:str)->bool:
+    def hasFile(self, file_name: str) -> bool:
         full_path = self.path + file_name
         return os.path.exists(full_path)
 
-    def get(self,file_name:str,key:str=None)->dict|list|int|str|float|bool|None:
+    def get(self, file_name: str, key: str = None) -> dict | list | int | str | float | bool | None:
         j = None
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
+            with open(self.getFileFullPath(file_name), "rb") as f:
                 j = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
@@ -33,37 +42,38 @@ class DataManager:
                 return j[key]
         return None
 
-    def findGet(self,file_name:str,key:str=None,dis_val=None):
-        r = self.get(file_name,key)
+    def findGet(self, file_name: str, key: str = None, dis_val=None):
+        r = self.get(file_name, key)
         if r == None:
             return dis_val
         return r
 
-    def set(self,file_name:str,key:str,val:dict|list|int|str|float|bool)->bool:
+    def set(self, file_name: str, key: str, val: dict | list | int | str | float | bool) -> bool:
         j = None
         raw_f = b""
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
+            with open(self.getFileFullPath(file_name), "rb") as f:
                 raw_f = f.read()
-                j:dict = json.loads(raw_f)
+                j: dict = json.loads(raw_f)
         except BaseException as e:
             logging.exception(e)
-        if j!=None:
+        if j != None:
             if type(j) == dict:
                 j[key] = val
                 try:
-                    with open(self.getFileFullPath(file_name),"wb") as f:
-                        f.write(json.dumps(j,ensure_ascii=False,indent=4).encode("utf8"))
+                    with open(self.getFileFullPath(file_name), "wb") as f:
+                        f.write(json.dumps(j, ensure_ascii=False,
+                                indent=4).encode("utf8"))
                     return True
                 except BaseException as e:
                     logging.exception(e)
         return False
 
-    def getMenbers(self, file_name:str, keys:None|list[str]|tuple[str]=None, dis_fnl:object=lambda x:None)->dict:
+    def getMenbers(self, file_name: str, keys: None | list[str] | tuple[str] = None, dis_fnl: object = lambda x: None) -> dict:
         j = None
         r = {}
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
+            with open(self.getFileFullPath(file_name), "rb") as f:
                 j = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
@@ -78,20 +88,21 @@ class DataManager:
                     r[i] = dis_fnl(i)
         return r
 
-    def setMenbers(self, file_name:str, key_vals:dict)->bool:
+    def setMenbers(self, file_name: str, key_vals: dict) -> bool:
         j = None
         try:
-            with open(self.getFileFullPath(file_name),"rb") as f:
-                j:dict = json.loads(f.read())
+            with open(self.getFileFullPath(file_name), "rb") as f:
+                j: dict = json.loads(f.read())
         except BaseException as e:
             logging.exception(e)
-        if j!=None:
+        if j != None:
             if type(j) == dict:
                 for key in key_vals:
                     j[key] = key_vals[key]
                 try:
-                    with open(self.getFileFullPath(file_name),"wb") as f:
-                        f.write(json.dumps(j,ensure_ascii=False,indent=4).encode("utf8"))
+                    with open(self.getFileFullPath(file_name), "wb") as f:
+                        f.write(json.dumps(j, ensure_ascii=False,
+                                indent=4).encode("utf8"))
                     return True
                 except BaseException as e:
                     logging.exception(e)
