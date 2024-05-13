959dcfc39d37de3c83a1eab56187be954cd0e812
LGS <piaoxuegd@gmail.com>
2024-05-14 00:13:30 +0800

    Add LLM chatbot and Piaoxue's group

---
+++ cqpy/LLMChat.py
@@ -0,0 +1,42 @@
+# test if the openai SDK is installed
+# if not, install it
+
+try:
+    from openai import OpenAI
+except ImportError:
+    import os
+    os.system("pip install openai")
+    from openai import OpenAI
+
+api_key = "sk-5662eda30f014fb39036aa0e1855269b"
+
+
+def createClient(api_key: str, base_url: str = "https://api.deepseek.com"):
+    return OpenAI(api_key=api_key, base_url=base_url)
+
+
+def get_response(client, message, model, max_tokens=4096, temperature=1, AI_role="system", user_role="user", AI_content="You are a helpful assistant"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": AI_role, "content": AI_content},
+            {"role": user_role, "content": message},
+        ],
+        max_tokens=max_tokens,
+        temperature=temperature
+    )
+
+    return response
+
+
+def extract_response(response, debug_mode=False) -> str:
+    if debug_mode:
+        extract_str = "debug mode: \n"
+        extract_str += "response content: " + response.choices[0].message.content + "\n"
+        extract_str += "token count: " + f'completion_tokens={response.usage.completion_tokens}, prompt_tokens={response.usage.prompt_tokens}, total_tokens={response.usage.total_tokens}\n'
+        extract_str += "finish reason: " + response.choices[0].finish_reason + "\n"
+    else:
+        extract_str = response.choices[0].message.content
+
+    
+    return extract_str
---
+++ cqpy/cqgroups/GPiaoXue.py
@@ -0,0 +1,69 @@
+from BaseGroupForHHZ import BaseGroupForHHZ
+from BaseGroup import BaseGroup
+from .I.IRollGroup import IRollGroup
+from .I.IItemGroup import IItemGroup
+from .I.ITTKGroup import ITTKGroup
+from ..GroupHelper import GroupHelper
+from ..LLMChat import *
+import base64
+
+class GPiaoXue(BaseGroupForHHZ, IRollGroup, IItemGroup, ITTKGroup):
+    def __init__(self):
+        super().__init__()
+        self.group_id = int(base64.b64decode(b'NTUzMDY2MTM0').decode())
+        self.clientArgs = {'api_key':None, 'base_url':'https://api.deepseek.com'}
+        self.chatArgs = {'model':'deepseek-chat', 'max_tokens':4096, 'temperature':1, 'AI_role':'system', 'user_role':'user', 'AI_content':'You are a helpful assistant'}
+        self.chatArgsType = {'model':str, 'max_tokens':int, 'temperature':float, 'AI_role':str, 'user_role':str, 'AI_content':str}
+        self.debug_mode = False
+
+        @BaseGroup.register
+        def chat(self, data, cmd):
+            if cmd.checkOrder("chat"):
+                if self.clientArgs['api_key'] is None:
+                    self.server.sendGroup(self.group_id, "API key is not set, please set it first.")
+                    return
+
+                message = cmd.getArg(1)
+
+                client = createClient(**self.clientArgs)
+                response = get_response(client, message, **self.chatArgs)
+                str = extract_response(response, self.debug_mode)
+
+                self.server.sendGroup(self.group_id, str)
+
+        @BaseGroup.register
+        def setAPIKey(self, data, cmd):
+            if cmd.checkOrder("setAPIKey"):
+                encodedStr = cmd.getArg(1)
+
+                try:
+                    decodedStr = base64.b64decode(encodedStr).decode()
+                except:
+                    self.server.sendGroup(self.group_id, "Invalid encoded API key.")
+                    return
+                
+                self.clientArgs['api_key'] = decodedStr
+                self.server.sendGroup(self.group_id, "API key set.")
+
+        @BaseGroup.register
+        def setChatArgs(self, data, cmd):
+            if cmd.checkOrder("setChatArgs"):
+                arg1, arg2 = cmd.getArg(1), cmd.getArg(2)
+
+                if (arg1 is None) or (arg2 is None):
+                    self.server.sendGroup(self.group_id, "Invalid chat args.")
+                    return
+                
+                if arg1 not in self.chatArgs:
+                    self.server.sendGroup(self.group_id, "Invalid chat args.")
+                    return
+
+                self.chatArgs[arg1] = self.chatArgsType[arg1](arg2)
+
+                self.server.sendGroup(self.group_id, f"Successfully set {arg1} to {arg2}.")
+        
+        @BaseGroup.register
+        def showChatArgs(self, data, cmd):
+            if cmd.checkOrder("showChatArgs"):
+                self.server.sendGroup(self.group_id, str(self.chatArgs))
+    
