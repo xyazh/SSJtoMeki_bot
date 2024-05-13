6d0ec700ac6d5b17b40e8247ade4ccba03f50a0b
xyazh <330237917@qq.com>
2024-05-14 00:40:00 +0800

    修复

--- cqpy/cqgroups/GPiaoXue.py
+++ cqpy/cqgroups/GPiaoXue.py
@@ -24,54 +24,53 @@ class GPiaoXue(BaseGroupForHHZ, IRollGroup, IItemGroup, ITTKGroup):
             arg = order.getArg(1)
             self.server.sendGroup(self.group_id, "%s发送的指令test的第一个参数为%s"%(name,arg))
 
-        @BaseGroup.register
-        def chat(self, data, cmd):
-            if cmd.checkOrder("chat"):
-                if self.clientArgs['api_key'] is None:
-                    self.server.sendGroup(self.group_id, "API key is not set, please set it first.")
-                    return
+    @BaseGroup.register
+    def chat(self, data, cmd):
+        if cmd.checkOrder("chat"):
+            if self.clientArgs['api_key'] is None:
+                self.server.sendGroup(self.group_id, "API key is not set, please set it first.")
+                return
 
-                message = cmd.getArg(1)
+            message = cmd.getArg(1)
 
-                client = createClient(**self.clientArgs)
-                response = get_response(client, message, **self.chatArgs)
-                str = extract_response(response, self.debug_mode)
+            client = createClient(**self.clientArgs)
+            response = get_response(client, message, **self.chatArgs)
+            str = extract_response(response, self.debug_mode)
 
-                self.server.sendGroup(self.group_id, str)
+            self.server.sendGroup(self.group_id, str)
 
-        @BaseGroup.register
-        def setAPIKey(self, data, cmd):
-            if cmd.checkOrder("setAPIKey"):
-                encodedStr = cmd.getArg(1)
+    @BaseGroup.register
+    def setAPIKey(self, data, cmd):
+        if cmd.checkOrder("setAPIKey"):
+            encodedStr = cmd.getArg(1)
 
-                try:
-                    decodedStr = base64.b64decode(encodedStr).decode()
-                except:
-                    self.server.sendGroup(self.group_id, "Invalid encoded API key.")
-                    return
-                
-                self.clientArgs['api_key'] = decodedStr
-                self.server.sendGroup(self.group_id, "API key set.")
+            try:
+                decodedStr = base64.b64decode(encodedStr).decode()
+            except:
+                self.server.sendGroup(self.group_id, "Invalid encoded API key.")
+                return
+            
+            self.clientArgs['api_key'] = decodedStr
+            self.server.sendGroup(self.group_id, "API key set.")
 
-        @BaseGroup.register
-        def setChatArgs(self, data, cmd):
-            if cmd.checkOrder("setChatArgs"):
-                arg1, arg2 = cmd.getArg(1), cmd.getArg(2)
+    @BaseGroup.register
+    def setChatArgs(self, data, cmd):
+        if cmd.checkOrder("setChatArgs"):
+            arg1, arg2 = cmd.getArg(1), cmd.getArg(2)
 
-                if (arg1 is None) or (arg2 is None):
-                    self.server.sendGroup(self.group_id, "Invalid chat args.")
-                    return
-                
-                if arg1 not in self.chatArgs:
-                    self.server.sendGroup(self.group_id, "Invalid chat args.")
-                    return
+            if (arg1 is None) or (arg2 is None):
+                self.server.sendGroup(self.group_id, "Invalid chat args.")
+                return
+            
+            if arg1 not in self.chatArgs:
+                self.server.sendGroup(self.group_id, "Invalid chat args.")
+                return
 
-                self.chatArgs[arg1] = self.chatArgsType[arg1](arg2)
+            self.chatArgs[arg1] = self.chatArgsType[arg1](arg2)
 
-                self.server.sendGroup(self.group_id, f"Successfully set {arg1} to {arg2}.")
-        
-        @BaseGroup.register
-        def showChatArgs(self, data, cmd):
-            if cmd.checkOrder("showChatArgs"):
-                self.server.sendGroup(self.group_id, str(self.chatArgs))
+            self.server.sendGroup(self.group_id, f"Successfully set {arg1} to {arg2}.")
     
+    @BaseGroup.register
+    def showChatArgs(self, data, cmd):
+        if cmd.checkOrder("showChatArgs"):
+            self.server.sendGroup(self.group_id, str(self.chatArgs))
