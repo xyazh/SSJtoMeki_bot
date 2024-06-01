db7f738206590eb414b202f61f701dd77fbebb8d
xyazh <330237917@qq.com>
2024-06-01 20:28:24 +0800

    推送

--- cqpy/cqgroups/GPiaoXue.py
+++ cqpy/cqgroups/GPiaoXue.py
@@ -9,33 +9,36 @@ from ..LLMChat import LLMAPI, DeepseekAPI, ERNIEAPI, QwenAPI
 import base64
 import pickle
 
+
 class GPiaoXue(BaseGroupForHHZ, IRollGroup, IItemGroup, ITTKGroup):
     def __init__(self):
         super().__init__()
         self.group_id = int(base64.b64decode(b'NTUzMDY2MTM0').decode())
         self.debug_mode = False
-        
+
         try:
-            self.data_manager:DataManager = DataManager("\\PiaoXue\\")
+            self.data_manager: DataManager = DataManager("\\PiaoXue\\")
             with self.data_manager.openFile('key.pkl', 'rb') as f:
                 key_dict = pickle.load(f)
+
+                
+                self.API_dict = {
+                    "DeepseekAPI": DeepseekAPI(key_dict['deepseek']),
+                    "ERNIEApi": ERNIEAPI(key_dict['ERNIE']['api_key'], key_dict['ERNIE']['secret_key']),
+                    "QwenAPI": QwenAPI(key_dict['Qwen'])}
+
+            self.activeAPI: LLMAPI = self.API_dict["QwenAPI"]
         except BaseException:
             pass
 
-        self.API_dict = {
-            "DeepseekAPI": DeepseekAPI(key_dict['deepseek']), 
-            "ERNIEApi": ERNIEAPI(key_dict['ERNIE']['api_key'], key_dict['ERNIE']['secret_key']), 
-            "QwenAPI": QwenAPI(key_dict['Qwen'])}
-        
-        self.activeAPI: LLMAPI = self.API_dict["QwenAPI"]
-
     @BaseGroup.register
     @BaseGroup.helpData(["o"], "测试指令", "test", "test (msg)", "测试指令")
-    def test(self,data,order):
+    def test(self, data, order):
         if order.checkOrder("test"):
             name = GroupHelper.getName(data)
             arg = order.getArg(1)
-            self.server.sendGroup(self.group_id, "%s发送的指令test的第一个参数为%s"%(name,arg))
+            self.server.sendGroup(
+                self.group_id, "%s发送的指令test的第一个参数为%s" % (name, arg))
 
     @BaseGroup.register
     def chat(self, data, cmd):
@@ -57,18 +60,19 @@ class GPiaoXue(BaseGroupForHHZ, IRollGroup, IItemGroup, ITTKGroup):
                 self.server.sendGroup(self.group_id, f"API已切换为{arg1}")
             else:
                 self.server.sendGroup(self.group_id, f"API {arg1} 不存在")
-                self.server.sendGroup(self.group_id, f"可用API有{', '.join(self.API_dict.keys())}")
-    
+                self.server.sendGroup(
+                    self.group_id, f"可用API有{', '.join(self.API_dict.keys())}")
+
     @BaseGroup.register
     def showAPIList(self, data, cmd):
         if cmd.checkOrder("showAPIList"):
             self.server.sendGroup(self.group_id, f"当前API为{self.activeAPI}")
-            self.server.sendGroup(self.group_id, f"可用API有{', '.join(self.API_dict.keys())}")
+            self.server.sendGroup(
+                self.group_id, f"可用API有{', '.join(self.API_dict.keys())}")
             message = "API介绍：\nDeepseekAPI：由deepseek公司开发的开源模型deepseek v2，性能优秀，推理成本低\nERNIEApi：由百度开发的ERNIE系列模型，4和3.5 \
                         性能不错，但是API贵。免费的speed和lite模型有点憨憨。\nQwenAPI：由阿里开发的Qwen模型，分闭源的qwen系列和开源的qwen1.5系列，性能优秀，送了一大堆免费额度，建议用这个\n"
             self.server.sendGroup(self.group_id, message)
 
-    
     @BaseGroup.register
     def changeModel(self, data, cmd):
         if cmd.checkOrder("changeModel"):
@@ -79,16 +83,20 @@ class GPiaoXue(BaseGroupForHHZ, IRollGroup, IItemGroup, ITTKGroup):
                 self.server.sendGroup(self.group_id, f"模型已切换为{arg1}")
             else:
                 self.server.sendGroup(self.group_id, f"模型{arg1}不存在")
-                self.server.sendGroup(self.group_id, f"可用模型有{', '.join(self.activeAPI.model_list)}")
+                self.server.sendGroup(
+                    self.group_id, f"可用模型有{', '.join(self.activeAPI.model_list)}")
 
     @BaseGroup.register
     def showModelList(self, data, cmd):
         if cmd.checkOrder("showModelList"):
-            self.server.sendGroup(self.group_id, f"当前模型为{self.activeAPI.model}")
-            self.server.sendGroup(self.group_id, f"可用模型有{', '.join(self.activeAPI.model_list)}")
+            self.server.sendGroup(
+                self.group_id, f"当前模型为{self.activeAPI.model}")
+            self.server.sendGroup(
+                self.group_id, f"可用模型有{', '.join(self.activeAPI.model_list)}")
 
     @BaseGroup.register
     def debugMode(self, data, cmd):
         if cmd.checkOrder("debugMode"):
             self.debug_mode = not self.debug_mode
-            self.server.sendGroup(self.group_id, f"debug模式已切换为{self.debug_mode}")
+            self.server.sendGroup(
+                self.group_id, f"debug模式已切换为{self.debug_mode}")
