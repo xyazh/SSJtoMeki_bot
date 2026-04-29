2fd351d0424e0fc75eb651b7db9e301126d81e1e
xyazh <330237917@qq.com>
2026-04-29 23:51:35 +0800

    push

--- mods/ModSherryiBangDream/commands/__init__.py
+++ mods/ModSherryiBangDream/commands/__init__.py
@@ -39,3 +39,47 @@ def searchSong(msg: PacketMsg, name: str = "", **kw):
         send_msg.updateImage(f"base64://{data}")
         Cqserver.instance.sendGroupMsg(send_msg, msg.group_id)
     threadWrapper(_searchSong)
+
+
+@Command(
+    "[e:emun('.','/','。')][e:emun('bg','bang','bgd','bangdream')][e:emun(' card','card ',' card ','card')][name:str]",
+    sign="bg card [name:str]",
+    desc="邦多利查卡",
+    category="BGD"
+)
+def searchCard(msg: PacketMsg, name: str = "", **kw):
+    if not name:
+        return
+    def _searchCard():
+        result = search_card([3, 0], text=name)
+        if not result:
+            return
+        card = result[0]
+        if card.get("type") != "base64":
+            return
+        data = card.get("string")
+        send_msg = MsgBuilder()
+        send_msg.updateImage(f"base64://{data}")
+        Cqserver.instance.sendGroupMsg(send_msg, msg.group_id)
+    threadWrapper(_searchCard)
+
+
+@Command(
+    "[e:emun('.','/','。')][e:emun('bg','bang','bgd','bangdream')][e:emun(' pop','pop ',' pop ','pop')][count:int] [id:int]",
+    sign="bg pop [count:int] [id:int]",
+    desc="邦多利模拟抽卡",
+    category="BGD"
+)
+def gachaSimulate(msg: PacketMsg, count: int = 10,id: int = 1, **kw):
+    def _gachaSimulated():
+        result = gacha_simulate(3, times=count, gacha_id=id)
+        if not result:
+            return
+        card = result[0]
+        if card.get("type") != "base64":
+            return
+        data = card.get("string")
+        send_msg = MsgBuilder()
+        send_msg.updateImage(f"base64://{data}")
+        Cqserver.instance.sendGroupMsg(send_msg, msg.group_id)
+    threadWrapper(_gachaSimulated)
